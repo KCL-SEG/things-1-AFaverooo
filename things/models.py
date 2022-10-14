5062b9daf5b98242473d960cc3b4d642387a6674
Augusto Favero <augusto.favero@kcl.ac.uk>
2022-10-14 16:08:51 +0100

    Add thing model and extend it to match requirements

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField(max_length = 30, unique = True, blank = False)
+    description = models.TextField(max_length = 120, blank = True, unique = False)
+    quantity = models.IntegerField(
+        validators = [MaxValueValidator(100),MinValueValidator(0)]
+    )
